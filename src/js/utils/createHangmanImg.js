6d5d13e5773bf695466f4cfa8a4205ac49db33a7
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-12-27 19:16:25 +0200

    fixed src for imgs

--- src/js/utils/createHangmanImg.js
+++ src/js/utils/createHangmanImg.js
@@ -1,6 +1,6 @@
 export const createHangmanImg = () => {
   const image = document.createElement('img');
-  image.src = '/images/hg-0.png';
+  image.src = './images/hg-0.png';
   image.id = 'hangman-img';
   image.classList.add('hangman-img');
   image.alt = 'hangman image';
